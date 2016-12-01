64e763f4f55b422f6222a0b4957cfc58b7d60bff
MsvOrca <wjp229@naver.com>
2016-12-01 13:51:55 +0900

    sensor

--- sensor2.c
+++ sensor2.c
@@ -6,9 +6,7 @@ typedef struct sensor
 {
 	float humi;
 	float temp;
-	float form;
 }sensor;
-
 int main(){
 	sensor save; //struct(I don't use array)
 	int i, j;
@@ -16,12 +14,12 @@ int main(){
 	char a[30];
 	char b;
 	int tmp;
-	FILE *stor = fopen("stor1.ino", "wt");; //save imformation to stor.
+	FILE *stor = fopen("stor2.ino", "wt");; //save imformation to stor.
 	while(1){
 		result='\0';
-		system("rm S1.html");
-		system("wget -O S1.html 192.168.123.101");
-		FILE *test = fopen("S1.html", "rt");
+		system("rm S2.html");
+		system("wget -O S2.html 192.168.123.100");
+		FILE *test = fopen("S2.html", "rt");
 		if(test==NULL)
 			return -1;
 		for(i=0; (b=fgetc(test))!='\n'; i++)
@@ -35,16 +33,10 @@ int main(){
 				fprintf(stor, "humidity : %.1f ", save.humi);
 				fflush(stor);
 				result = strtok(NULL, ",");}
-			else if(i==1){
-				save.temp=atof(result);
-				printf("temperature : %.1f\n", save.temp);
-				fprintf(stor, "temperature : %.1f ", save.temp);
-				fflush(stor);
-				result = strtok(NULL, ",");}
 			else {
 				save.temp=atof(result);
-				printf("formaldehyde : %.1f\n", save.temp);
-				fprintf(stor, "formaldehyde : %.1f\n", save.temp);
+				printf("temperature : %.1f\n", save.temp);
+				fprintf(stor, "temperature : %.1f\n", save.temp);
 				fflush(stor);
 				result = strtok(NULL, ",");}
 		}
